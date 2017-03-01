07d05fa82dcfe4d5c79cfe4973149dc33c472202
Brian Bycynski <high5bri@hotmail.com>
2017-03-01 08:21:43 -0500

    just starting

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,45 @@
 
+class TimersDashboard extends React.Component {
+  render() {
+    return (
+      <div className='ui three column centered grid'>
+        <div className='column'>
+          <EditableTimerList />
+          <ToggleableTimerForm
+          isOpen={true}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+class EditableTimerList extends React.Component {
+  render() {
+    return (
+      <div id='timers'>
+        <EditableTimer
+          title='Learn React'
+          project='Web D'
+          elapsed='8986300'
+          runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimer
+          title='Learn extreme ironing'
+          project='World Domination'
+          elapsed='3890985'
+          runningSince={null}
+          editFormOpen={true}
+        />
+      </div>
+    )
+  }
+}
+class EditableTimer extends React.Component {
+  render() {
+    if (this.props.editFormOpen) {
+      return
+    }
+  }
+}
---
+++ public/js/order-hierachy.js
@@ -0,0 +1,7 @@
+• TimersDashboard (green): Parent container
+  – EditableTimerList (red): Displays a list of timer containers
+    * EditableTimer (aqua): Displays either a timer or a timer’s edit form
+      · Timer (yellow): Displays a given timer
+      · TimerForm (blue): Displays a given timer’s edit form
+  – ToggleableTimerForm (purple): Displays a form to create a new timer
+    * TimerForm (not displayed): Displays a new timer’s create form
