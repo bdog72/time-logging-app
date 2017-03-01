963d02103c87b3e69c643a29f76d3ab6180ca393
Brian Bycynski <high5bri@hotmail.com>
2017-03-01 11:26:21 -0500

    static app at this point

--- public/js/app.js
+++ public/js/app.js
@@ -86,3 +86,54 @@ class TimerForm extends React.Component {
     );
   }
 }
+class ToggleableTimerForm extends React.Component {
+  render() {
+    if (this.props.isOpen) {
+      return (
+        <TimerForm />
+      );
+    } else {
+      return (
+        <div className='ui basic content center aligned segment'>
+          <button className='ui basic button icon'>
+            <i className='plus icon'/>
+          </button>
+        </div>
+      );
+    }
+  }
+}
+class Timer extends React.Component {
+  render() {
+    const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+    return (
+      <div className='ui centered card'>
+        <div className='content'>
+          <div className='header'>
+            {this.props.title}
+          </div>
+          <div className='meta'>
+            {this.props.project}
+          </div>
+          <div className='center aligned description'>
+            <h2>
+              {elapsedString}
+            </h2>
+          </div>
+          <div className='extra content'>
+            <span className='right floated edit icon'>
+              <i className='trash icon' />
+            </span>
+          </div>
+        </div>
+        <div className='ui bottom attached blue basic button'>
+          start
+        </div>
+      </div>
+    );
+  }
+}
+ReactDOM.render(
+  <TimersDashboard />,
+  document.getElementById('content')
+);
